2324e6e23fbe4006018f935651376eb39e8a9f9d
Robert Peteuil <robert@peteuil.com>
2019-06-14 17:34:34 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Palace-Arcade
+
+A simple demo of Terraform with Azure Container Apps.
+
+Can be used to demonstrate the use of Terraform Enterprise Private Modules and Sentinel policies.
---
+++ main.tf
@@ -0,0 +1,46 @@
+# VARIABLES
+
+# Public URL Prefix = http://PREFIX.azurewebsites.net/
+variable "prefix" {
+  default = "robpco-arcade"
+}
+
+# Change to robpco/pizzaplanet or palacearcade
+variable "image" {
+  default = "robpco/palacearcade"
+}
+
+# Choose a location
+variable "location" {
+  default = "centralus"
+}
+
+variable "https_only" {
+  default = "false"
+}
+
+# Azure Infrastrcture
+provider "azurerm" {}
+
+# Create Resource Group
+resource "azurerm_resource_group" "arcade" {
+  name     = "${var.prefix}-containerapp-demo"
+  location = "${var.location}"
+}
+
+# Utilize the web_app_container module from TFE Private Registry
+module "web_app_container" {
+  source  = "app.terraform.io/multicloud/web-app-container/azurerm"
+  version = "1.5.0"
+
+  name                = "${var.prefix}"
+  resource_group_name = "${azurerm_resource_group.arcade.name}"
+  container_image     = "${var.image}"
+  container_type      = "docker"
+  https_only          = "${var.https_only}"
+  port                = "80"
+}
+
+output "container_app_url" {
+  value = "https://${module.web_app_container.hostname}"
+}
