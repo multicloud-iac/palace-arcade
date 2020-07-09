d34080a94c233d7674ada38e7f3e6b4d208dcfe0
Robert Peteuil <robert@peteuil.com>
2020-07-09 13:28:13 -0700

    change module version

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "azurerm_resource_group" "arcade" {
 # Utilize the web_app_container module from TFE Private Registry
 module "web_app_container" {
   source  = "app.terraform.io/multicloud/web-app-container/azurerm"
-  version = "2.6.2"
+  version = "2.6.3"
 
   name                = var.prefix
   resource_group_name = azurerm_resource_group.arcade.name
